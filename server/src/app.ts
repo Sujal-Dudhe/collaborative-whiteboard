136bd77a36fa64f650d397412819c1103e0e7611
Sujal Dudhe <sujaldudhe2004@gmail.com>
2026-06-30 23:41:53 +0530

    feat: add CORS middleware to Express routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import passport from 'passport'
 
 import './config/passport'
@@ -8,6 +9,11 @@ import authRoutes from './routes/auth.route'
 import roomRoutes from './routes/room.route'
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
 
 app.use(express.json())
 app.use(passport.initialize())
